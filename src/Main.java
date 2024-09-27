3c13ca02bceb9cee50ed088ff1d48c82d1b6fb6b
Mariofan765 <mariofan7652003@yandex.ru>
2024-09-27 20:26:16 +0300

    init

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,41 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
+    private int[] array = new int[3];
+
+    public void addNumber(int number) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == 0) {
+                array[i] = number;
+                return;
+            } else if (number < array[i]) {
+                array[i] = number;
+                break;
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Введите числа через пробел");
         Scanner scanner = new Scanner(System.in);
+        Main memoryCell = new Main();
+
+        System.out.println("Введите числа через пробел");
         String input = scanner.nextLine().trim();
-        if (input.isEmpty()) {
-            System.out.println("Ввод пустой");
-            return;
+        String[] numbers = input.split(" ");
+
+        for (String numStr : numbers) {
+            try {
+                int number = Integer.parseInt(numStr);
+                memoryCell.addNumber(number);
+            } catch (NumberFormatException e) {
+            }
         }
 
-        String[] numbers = input.split(" ");
-        int[] array = Arrays.stream(numbers).mapToInt(Integer::parseInt).toArray();
-        int result = 0;
-        for (int number : array) {
-            result += number*number;
+        System.out.println("Значения в ячейке");
+        for (int value : memoryCell.array) {
+            if (value != 0) {
+                System.out.print(value + " ");
+            }
         }
-        System.out.println(result);
     }
 }
